bd1aac7c23277c81c4c3bc1c9d155567e136642b
Vu Huy Hai <vuhuyhai1102@gmail.com>
2022-05-25 17:24:27 +0700

    chart and filter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import './Header.css';
 import Logo from '../../img/Logo.png';
 import { Button, Typography } from '@mui/material';
-import { KeyboardArrowDown } from '@mui/icons-material';
-import { indigo } from '@mui/material/colors';
+import {
+  KeyboardArrowDown,
+  ArrowRightAlt,
+  PeopleAlt
+} from '@mui/icons-material';
+import { indigo, deepPurple, blue } from '@mui/material/colors';
+import { useAppSelector } from '../../app/hooks';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useAppSelector((state) => state.user.value);
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
   return (
     <div className="header-container">
       <div className="header-brand">
@@ -37,7 +49,7 @@ const Header = () => {
               Đăng ký tiêm
             </a>
           </li>
-          <li className="header-menu__item">
+          <li className="header-menu__item more">
             <a
               href="/search"
               className="header-menu__link"
@@ -47,6 +59,85 @@ const Header = () => {
               <span>Tra cứu</span>
               <KeyboardArrowDown />
             </a>
+
+            <div className="header-menu__more">
+              <ul className="header-menu__more-list">
+                <li className="header-menu__more-item">
+                  <div className="header-menu__more-icon">
+                    <PeopleAlt
+                      sx={{
+                        color: blue[600]
+                      }}
+                    />
+                  </div>
+
+                  <span className="header-menu__more-text">
+                    <Typography
+                      sx={{
+                        fontSize: '16px',
+                        fontWeight: '400',
+                        lineHeight: '24px',
+                        letterSpacing: '-0.04px',
+                        color: 'rgba(0, 0, 0, 0.87)'
+                      }}>
+                      Tra cứu chứng nhận tiêm
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontSize: '12px',
+                        fontWeight: '400',
+                        lineHeight: '18px',
+                        letterSpacing: '-0.04px',
+                        color: 'rgba(0, 0, 0, 0.87)'
+                      }}>
+                      Cập nhật nhanh và chính xác nhất
+                    </Typography>
+                  </span>
+                  <ArrowRightAlt
+                    sx={{
+                      color: blue[600]
+                    }}
+                  />
+                </li>
+                <li className="header-menu__more-item">
+                  <div className="header-menu__more-icon">
+                    <PeopleAlt
+                      sx={{
+                        color: deepPurple[600]
+                      }}
+                    />
+                  </div>
+
+                  <span className="header-menu__more-text">
+                    <Typography
+                      sx={{
+                        fontSize: '16px',
+                        fontWeight: '400',
+                        lineHeight: '24px',
+                        letterSpacing: '-0.04px',
+                        color: 'rgba(0, 0, 0, 0.87)'
+                      }}>
+                      Tra cứu kết quả đăng ký
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontSize: '12px',
+                        fontWeight: '400',
+                        lineHeight: '18px',
+                        letterSpacing: '-0.04px',
+                        color: 'rgba(0, 0, 0, 0.87)'
+                      }}>
+                      Cập nhật nhanh và chính xác nhất
+                    </Typography>
+                  </span>
+                  <ArrowRightAlt
+                    sx={{
+                      color: deepPurple[600]
+                    }}
+                  />
+                </li>
+              </ul>
+            </div>
           </li>
           <li className="header-menu__item">
             <a href="/document" className="header-menu__link">
@@ -54,21 +145,36 @@ const Header = () => {
             </a>
           </li>
         </ul>
-        <Button
-          sx={{
-            backgroundColor: '#fff',
-            color: indigo[700],
-            borderRadius: '8px 8px 8px 0',
-            padding: '8px 22px',
-            border: '1px solid rgba(0,0,0,0)',
-            '&:hover': {
-              backgroundColor: 'rgba(0,0,0,0)',
-              border: '1px solid #fff',
-              color: '#fff'
-            }
-          }}>
-          Đăng nhập
-        </Button>
+        {user.email ? (
+          <Typography
+            sx={{
+              fontSize: '16px',
+              fontWeight: '500',
+              lineHeight: '24px',
+              letterSpacing: '-0.04px',
+              color: '#fff',
+              cursor: 'pointer'
+            }}>
+            {user.email}
+          </Typography>
+        ) : (
+          <Button
+            sx={{
+              backgroundColor: '#fff',
+              color: indigo[700],
+              borderRadius: '8px 8px 8px 0',
+              padding: '8px 22px',
+              border: '1px solid rgba(0,0,0,0)',
+              '&:hover': {
+                backgroundColor: 'rgba(0,0,0,0)',
+                border: '1px solid #fff',
+                color: '#fff'
+              }
+            }}
+            onClick={handleLogin}>
+            Đăng nhập
+          </Button>
+        )}
       </div>
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Chart from '../components/Chart/Chart';
 import Composite from '../components/Composite/Composite';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import Location from '../components/Location/Location';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Composite />
+      <Chart />
+      <Location />
       <Footer />
     </div>
   );
