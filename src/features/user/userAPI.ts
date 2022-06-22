171a6c31cba2273bae967a28f81efb40fb1dfd9c
Vu Huy Hai <vuhuyhai1102@gmail.com>
2022-06-22 15:04:57 +0700

    remove unutilized code

--- src/features/user/userAPI.ts
+++ src/features/user/userAPI.ts
@@ -1,16 +1,3 @@
-// import { UserInfo } from './userSlice';
-
-// export function fetchUser(
-//   info = {
-//     email: '',
-//     password: ''
-//   }
-// ) {
-//   return new Promise<{ data: UserInfo }>((resolve) =>
-//     setTimeout(() => resolve({ data: info }), 2000)
-//   );
-// }
-
 export function fetchForgotPass(info = '') {
   return new Promise<{ data: string }>((resolve) =>
     setTimeout(() => resolve({ data: info }), 2000)
